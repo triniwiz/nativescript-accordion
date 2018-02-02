9eb6a743af9b313bbb7dfa7f4990f5bf83a179c0
westlakem <westlakem@users.noreply.github.com>
2018-02-02 13:36:57 -0500

    Update index.ts

--- angular/index.ts
+++ angular/index.ts
@@ -121,7 +121,7 @@ export class AccordionComponent {
         }
         if (needDiffer && !this._differ && isListLikeIterable(value)) {
             this._differ = this._iterableDiffers.find(this._items)
-                .create(this._cdr, (_index, item) => { return item; });
+                .create(null, (_index, item) => { return item; });
         }
 
         this.accordion.items = this._items;
